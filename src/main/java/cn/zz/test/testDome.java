3f0b688b7a242aab21819b8546af2d871d1f3486
张喆 <1804074178@qq.com>
2021-05-25 09:59:37 +0800

    B组第一次提交

--- src/main/java/cn/zz/test/testDome.java
+++ src/main/java/cn/zz/test/testDome.java
@@ -3,5 +3,7 @@ package cn.zz.test;
 public class testDome {
     public static void main(String[] args) {
         System.out.println("groupA提交了一段代码。。。。");
+
+        System.out.println("groupB提交了一段代码。。。。。");
     }
 }
